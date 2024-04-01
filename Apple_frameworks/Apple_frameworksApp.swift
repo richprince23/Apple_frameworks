21722cb3f8881ee4475527365d261b3230e64225
= <=>
2024-04-01 01:47:16 +0000

    Initial Commit

---
+++ Apple_frameworks/Apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_frameworksApp.swift
+//  Apple_frameworks
+//
+//  Created by Richard Kweku Aikins  on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
