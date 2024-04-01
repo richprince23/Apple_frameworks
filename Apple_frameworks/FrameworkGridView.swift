1dbe5230be4e6aa8f5bd7fd3995b66102634b6b1
= <=>
2024-04-01 03:00:31 +0000

    refactored mygriditem

--- Apple_frameworks/FrameworkGridView.swift
+++ Apple_frameworks/FrameworkGridView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    let cols: [GridItem] = [GridItem(.flexible()),
+                            GridItem(.flexible()),
+                            GridItem(.flexible()),
+    ];
+    var frameworkList : [Framework] = MockData.frameworks;
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: cols) {
+    //            MyGridItem(title: "AR Kit", icon: "arkit")
+    //            MyGridItem(title: "App Clips", icon: "app-clip")
+    //            MyGridItem(title: "App Clips", icon: "app-clip")
+    //            MyGridItem(title: "App Clips", icon: "app-clip")
+                ForEach(frameworkList, id: \.id){data in
+                    MyGridItem(data: data)
+                    
+                }
+                    
+    //        
+                
+            }
+        }
     }
 }
 
 #Preview {
     FrameworkGridView()
+}
+
+struct MyGridItem : View{
+    var data : Framework;
+    
+    var body : some View{
+        VStack(spacing: 10){
+            Image("\(data.imageName)")
+                .resizable()
+                .frame(width: 64, height: 64)
+            
+            Text(data.name)
+                .font(.system(size: 18, weight: .medium))
+                .scaledToFit()
+                .minimumScaleFactor(0.65)
+        }
+    }
 }
